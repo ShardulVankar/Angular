4b7d2be8f3894a66dbde2d7b72f1ad100e2a4399
ShardulVankar <37528462+ShardulVankar@users.noreply.github.com>
2019-08-04 09:54:06 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { BlogsComponent } from './blogs/blogs.component';
-import { InfoCreateComponent } from './info-create/info-create.component';
-import { InfoEditComponent } from './info-edit/info-edit.component';
+
 import { InfoViewComponent } from './info-view/info-view.component';
 import { InfoService } from './info.service';
 import { HttpInfoService } from './http-info.service';
@@ -18,8 +15,7 @@ import { IntervalInfoViewComponent } from './interval-info-view/interval-info-vi
 var Routes1 = [
   {path: 'home', component:HomeComponent},
   {path:'', redirectTo:'home', pathMatch:'full'},
-  {path:'aboutUs', component:AboutUsComponent},
-  {path:'Create', component:InfoCreateComponent},
+  
   {path:'info-view/:url', component:InfoViewComponent},
   {path:'interval-info-view/:character',component:IntervalInfoViewComponent}
   ];
@@ -28,10 +24,7 @@ var Routes1 = [
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutUsComponent,
-    BlogsComponent,
-    InfoCreateComponent,
-    InfoEditComponent,
+   
     InfoViewComponent,
     IntervalInfoViewComponent
   ],
